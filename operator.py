3003a85cd4d7eca52c8e2966e5c45f7d65474256
Vasoyakruti <vasoyakruti@gmail.com>
2026-01-27 09:10:13 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,48 @@
+#1.arithmetic operator
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+
+#2.,assignment operator
+x=10
+print(x)
+
+#3.unary minus
+a=5
+print(-a)
+
+#4.relational operators
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+#5.logical operators
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+#6.boolean 
+is_pass=True
+print(is_pass)
+
+#7.bitwise operators
+a=5
+b=3
+print(a&b)
+print(a|b)
+
+#8.membership operators
+numbers=[1,2,3,4]
+print(2 in numbers)
+print(5 is not numbers)
+
+#9.identity operators
+x=10
+y=10
+print(x is y)
+print(x is not y)
