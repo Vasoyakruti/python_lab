b572abeb1f968ba2ee9d14512ad2db4e4204039a
Vasoyakruti <vasoyakruti@gmail.com>
2026-01-31 08:17:38 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,9 @@
+#output using print()
+print("hello world")
+print("welcomw to atmiya university")
+
+#input using input()
+name = input("Enter your name:")
+print("hello",name)
+
+age = input("Enter your age:")
