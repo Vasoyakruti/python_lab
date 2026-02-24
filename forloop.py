519337da5eeca6aba8d00d7d3ecc42650fde7171
Vasoyakruti <vasoyakruti@gmail.com>
2026-02-24 08:08:57 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,24 @@
+for i in range(1,11):
+    print(i)
+
+for i in range(1,11):
+    if i%2==0:
+        print(i)
+
+for i in range(1,11):
+    if i%2!=0:
+        print(i )       
+
+for i in range(1,11):
+    print("4 x",i,"=",4*i)
+
+total=0
+for i in range(1,11):
+    total=total+i
+
+print("sum is:",total)
+
+number = [10,20,30,40]
+
+for n in number:
+    print(n)
